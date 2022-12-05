df2757d8b776fca745fa562e80f879135cb291dc
Joel Pascual <joeltrabajo92@gmail.com>
2022-12-05 12:33:12 +0100

    new file:   LucaTickets-Search/.gitignore 	new file:   LucaTickets-Search/.mvn/wrapper/maven-wrapper.jar 	new file:   LucaTickets-Search/.mvn/wrapper/maven-wrapper.properties 	new file:   LucaTickets-Search/mvnw 	new file:   LucaTickets-Search/mvnw.cmd 	new file:   LucaTickets-Search/pom.xml 	new file:   LucaTickets-Search/src/main/java/com/proyecto/spring/event/LucaTicketsSearchApplication.java 	new file:   LucaTickets-Search/src/main/java/com/proyecto/spring/event/controller/README 	new file:   LucaTickets-Search/src/main/java/com/proyecto/spring/event/feignEvents/README 	new file:   LucaTickets-Search/src/main/java/com/proyecto/spring/event/model/response/README 	new file:   LucaTickets-Search/src/main/java/com/proyecto/spring/event/service/README 	new file:   LucaTickets-Search/src/main/resources/application.properties 	new file:   LucaTickets-Search/src/test/java/com/proyecto/spring/event/LucaTicketsSearchApplicationTests.java
    Estructura básica del microservicio de búsqueda

---
+++ LucaTickets-Search/src/main/java/com/proyecto/spring/event/LucaTicketsSearchApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.spring.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaTicketsSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaTicketsSearchApplication.class, args);
+	}
+
+}
---
+++ LucaTickets-Search/src/test/java/com/proyecto/spring/event/LucaTicketsSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.spring.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaTicketsSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
